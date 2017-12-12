494a0b2689b59a79047bfb51bfbe0533108ce6d2
SAGARWORKLAPTOP\Sagar <sagar41196@gmail.com>
2017-12-12 16:16:07 -0500

    started replay UI

--- app/src/main/java/com/cs213/androidproject/chess_android_56/Controller/Replay.java
+++ app/src/main/java/com/cs213/androidproject/chess_android_56/Controller/Replay.java
@@ -2,21 +2,26 @@ package com.cs213.androidproject.chess_android_56.Controller;
 
 import android.os.Bundle;
 
+import com.cs213.androidproject.chess_android_56.Model.Board;
+import com.cs213.androidproject.chess_android_56.Model.Square;
 import com.cs213.androidproject.chess_android_56.R;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 /**
  * Created by Sagar on 12/11/2017.
@@ -27,13 +32,17 @@ import java.util.ArrayList;
 public class Replay extends AppCompatActivity {
     public static String gameName;
     public ArrayList<String> moves = new ArrayList<String>();
+    Board b= new Board();
+    static int ia=0;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_replay);
         System.out.println(gameName);
         Context context=this.getApplicationContext();
         String ret="";
-        ArrayList<String> moves = new ArrayList<>();
+        String game = "";
+        ArrayList<String> moves=new ArrayList<>();
+        System.out.println(ia);
         try {
 
             InputStream inputStream = context.openFileInput("gameHistory.txt");
@@ -55,8 +64,12 @@ public class Replay extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        moves=getMoves(ret);
-
+        game=getGame(ret);
+        moves=getMoves(game);
+        b.initBoard();
+        for(int i=0;i<moves.size();i++){
+            System.out.println(moves.get(i));
+        }
 
     }
 
@@ -64,14 +77,14 @@ public class Replay extends AppCompatActivity {
     public void ImageOnClick(View v){
         return ;
     }
-    public ArrayList<String> getMoves(String s){
+    public String getGame(String s){
         ArrayList<String> L=new ArrayList<>();
         int begin=0;
         int movebegin;
         String q="";
         if(s.contains(gameName)){
             begin=s.indexOf(gameName);
-            System.out.println(begin);
+           // System.out.println(begin);
         }
         movebegin=s.indexOf('~',begin);
         boolean parse=false;
@@ -83,7 +96,108 @@ public class Replay extends AppCompatActivity {
 
         }
         System.out.println(q);
-        return L;
+        return q;
+    }
+    public ArrayList<String> getMoves(String s){
+        ArrayList<String>m=new ArrayList<>();
+        String move="" ;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='|'){
+                System.out.println(move);
+                m.add(move);
+                move="";
+            }
+            else {
+                move += s.charAt(i);
+            }
+        }
+        return m ;
     }
 
+    private void refresh(Board board) {
+        Square[][] s = board.getBoard();
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                Square temp = s[i][j];
+                char file = 'a';
+                file += j;
+                int rank = 8 - i;
+                String id = file + "" + rank;
+                int ID = getResId(id, R.id.class);
+                Log.i("Position:", "" + file + rank);
+                ImageView img = (ImageView) findViewById(ID);
+
+                if (temp.getPieceType() == null || temp.getPieceType().length() == 0) {
+                    img.setImageResource(android.R.color.transparent);
+                }
+                else if (temp.getPieceType().equals("R")) {
+                    if (temp.getPiece().isWhite()) {
+                        img.setImageResource(R.drawable.whiterook);
+                    } else {
+                        img.setImageResource(R.drawable.blackrook);
+                    }
+                }
+                else if(temp.getPieceType().equals("N")){
+                    if(temp.getPiece().isWhite()){
+                        img.setImageResource(R.drawable.whiteknight);
+                    }
+                    else{
+                        img.setImageResource(R.drawable.blackknight);
+                    }
+                }
+                else if(temp.getPieceType().equals("R")){
+                    if(temp.getPiece().isWhite()){
+                        img.setImageResource(R.drawable.whiterook);
+                    }
+                    else{
+                        img.setImageResource(R.drawable.blackrook);
+                    }
+                }
+                else if(temp.getPieceType().equals("B")){
+                    if(temp.getPiece().isWhite()){
+                        img.setImageResource(R.drawable.whitebishop);
+                    }
+                    else{
+                        img.setImageResource(R.drawable.blackbishop);
+                    }
+                }
+                else if(temp.getPieceType().equals("Q")){
+                    if(temp.getPiece().isWhite()){
+                        img.setImageResource(R.drawable.whitequeen);
+                    }
+                    else{
+                        img.setImageResource(R.drawable.blackqueen);
+                    }
+                }
+                else if(temp.getPieceType().equals("p")){
+                    if(temp.getPiece().isWhite()){
+                        img.setImageResource(R.drawable.whitepawn);
+                    }
+                    else{
+                        img.setImageResource(R.drawable.blackpawn);
+                    }
+                }
+            }
+        }
+    }
+
+    public static int getResId(String resName, Class<?> c) {
+        try {
+            Field idField = c.getDeclaredField(resName);
+            return idField.getInt(idField);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public void onClickNextButton(View v){
+        System.out.println(ia);
+        String parts=moves.get(ia).substring(0,2);
+        String parts2=moves.get(ia).substring(4,5);
+        b.getSquare(parts).getPiece().move(parts,parts2,b);
+        ia++;
+        refresh(b);
+    }
 }
